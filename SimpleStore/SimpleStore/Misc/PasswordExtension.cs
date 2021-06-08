871dac476c58ebdbedce7b363c2873651148b343
ikondratyev <kondratyev.ie@gmail.com>
2021-06-08 21:45:20 +0300

    добавил расширения для засолки паролей

---
+++ SimpleStore/SimpleStore/Misc/PasswordExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleStore.Misc
+{
+    public  static class PasswordExtension
+    {
+        /// <summary>
+        /// Получить хэщ засоленного пароля
+        /// </summary>
+        /// <param name="password">пароль</param>
+        /// <param name="salt">соль</param>
+        /// <returns>Возвращает хэш засоленного пароля в нижнем регистре </returns>
+        private static string GetHashFromSalPassword(string password, string salt)
+        {
+            var checkArguments = string.IsNullOrEmpty(password) || string.IsNullOrEmpty(salt);
+            Contract.Requires<ArgumentNullException>(!checkArguments, "Password or salt is null");
+            var saltPassword = password + salt;
+            string resultHash;
+            using (var shaManager = new SHA256Managed())
+            {
+                var bytes = Encoding.UTF8.GetBytes(saltPassword);
+                var hash = shaManager.ComputeHash(bytes);
+                resultHash = BitConverter.ToString(hash).Replace("-", string.Empty).ToLowerInvariant();
+            }
+            return resultHash;
+        }
+    }
+}
