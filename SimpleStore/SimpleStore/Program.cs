dbee2a0e9ba426471150152d2eaaab3045c9ef6c
ikondratyev <kondratyev.ie@gmail.com>
2021-06-08 23:07:18 +0300

    Добавил авто миграцию и инициализацию дефолтными значениями

--- SimpleStore/SimpleStore/Program.cs
+++ SimpleStore/SimpleStore/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SimpleStore.Database;
+using SimpleStore.Misc;
 
 namespace SimpleStore
 {
@@ -13,14 +14,34 @@ namespace SimpleStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+                    context.Database.MigrateAsync();
+                    if (!context.Users.Any())
+                    {
+                        DefaultUserInitializer.Initialize(context);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "DbContextError");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseKestrel();
                 });
     }
 }
