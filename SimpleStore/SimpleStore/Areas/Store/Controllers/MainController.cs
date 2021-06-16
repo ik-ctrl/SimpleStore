fd0ffef6290924579a04af2fccd3a0c5b9d84504
ikondratyev <kondratyev.ie@gmail.com>
2021-06-16 21:07:05 +0300

    Разнес файлы по областям

---
+++ SimpleStore/SimpleStore/Areas/Store/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleStore.Areas.Store.Controllers
+{
+    [Area("Store")]
+    public class MainController:Controller
+    {
+        [Route("[area]")]
+        [Route("[area]/[controller]")]
+        [Route("[area]/[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ SimpleStore/SimpleStore/Controllers/RedirectController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleStore.Controllers
+{
+    public class RedirectController : Controller
+    {
+        private readonly ILogger<RedirectController> _logger;
+
+        public RedirectController(ILogger<RedirectController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///  Метод для перенаправления на основную страницу магазина
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            try
+            {
+                return Redirect("store/main/index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Redirect is fail", ex);
+                throw;
+            }
+
+        }
+    }
+}
