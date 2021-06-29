cfd7be410f2ec4399923403da3d977df2d7f01f7
ikondratyev <kondratyev.ie@gmail.com>
2021-06-29 23:19:16 +0300

    добавил конвертер из обычных моделей  в модели представления

---
+++ SimpleStore/SimpleStore/Models/Misc/ModelConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using SimpleStore.Database.DAL;
+using SimpleStore.ViewModels.StoreViewModels;
+
+namespace SimpleStore.Models.Misc
+{
+    /// <summary>
+    /// Конвертер модель в модель представления
+    /// </summary>
+    public static class ModelConverter
+    {
+
+        /// <summary>
+        /// Конвертирует список моделей продукта в список моделей представления продукта 
+        /// </summary>
+        /// <param name="products"> Список моделей продуктов</param>
+        /// <returns></returns>
+        public static IEnumerable<ProductViewModel> ProductsToProductViewModels(IEnumerable<Product> products)
+        {
+            var productsVM = new List<ProductViewModel>();
+            foreach (var product in products)
+            {
+                productsVM.Add(new ProductViewModel(product));
+            }
+            return productsVM;
+        }
+    }
+}
