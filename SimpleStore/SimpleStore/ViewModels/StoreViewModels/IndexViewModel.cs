cb7d88d0429a8a23bcdf0c9a4bc039bfdcb7dc65
ikondratyev <kondratyev.ie@gmail.com>
2021-06-29 23:18:11 +0300

    добавил модели представления для отображения продуктов и странице index

---
+++ SimpleStore/SimpleStore/ViewModels/StoreViewModels/IndexViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleStore.ViewModels.StoreViewModels
+{
+    /// <summary>
+    /// Модель представления для Index.html
+    /// </summary>
+    public class IndexViewModel
+    {
+
+        public IndexViewModel(IEnumerable<ProductViewModel> productViewModels,int currentPageNumber,int finalPageNumber)
+        {
+            //todo: может стоит сразу передавать  лист?
+            var viewModels = productViewModels.ToList();
+            Products = viewModels;
+            ProductsCount = viewModels.Count();
+            CurrentPageNumber = currentPageNumber;
+            FinalPageNumber = finalPageNumber;
+        }
+
+        /// <summary>
+        /// Список отображаемых продуктов
+        /// </summary>
+        public IEnumerable<ProductViewModel> Products { get; }
+
+        /// <summary>
+        /// Количество продуктов для отображения
+        /// </summary>
+        public int ProductsCount { get; }
+
+        /// <summary>
+        /// Номер текущей страницы
+        /// </summary>
+        public int CurrentPageNumber { get; }
+        
+        /// <summary>
+        /// Номер последней страницы
+        /// </summary>
+        public int FinalPageNumber { get; }
+
+    }
+}
---
+++ SimpleStore/SimpleStore/ViewModels/StoreViewModels/ProductViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleStore.Database.DAL;
+
+namespace SimpleStore.ViewModels.StoreViewModels
+{
+    public class ProductViewModel
+    {
+        public readonly Product _product;
+        
+        public ProductViewModel(Product product)
+        {
+            _product = product;
+        }
+
+        public double Price => _product.Price;
+        public string Name => _product.Name;
+
+        public string Description => _product.Description;
+
+        public double Discount => _product.Discount;
+
+        public double ProductCount => _product.ProductCount;
+
+        public ICollection<ProductImage> Images => _product.Images;
+
+        public override string ToString()
+        {
+            return $"{_product.Id}|{_product.Name}|{_product.Price}";
+        }
+    }
+}
