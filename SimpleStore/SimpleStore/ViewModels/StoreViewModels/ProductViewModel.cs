24c582bbfe14feb8a0b805249dd6a3cd34c729f0
KondratievIE <kondratyev.ie@gmail.com>
2021-07-05 12:59:35 +0300

    добавил комментарии/ сделал поле приватным

--- SimpleStore/SimpleStore/ViewModels/StoreViewModels/ProductViewModel.cs
+++ SimpleStore/SimpleStore/ViewModels/StoreViewModels/ProductViewModel.cs
@@ -1,30 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
 using SimpleStore.Database.DAL;
 
 namespace SimpleStore.ViewModels.StoreViewModels
 {
     public class ProductViewModel
     {
-        public readonly Product _product;
+        private readonly Product _product;
         
         public ProductViewModel(Product product)
         {
             _product = product;
         }
 
+        /// <summary>
+        /// Цена товара
+        /// </summary>
         public double Price => _product.Price;
+
+        /// <summary>
+        /// Название товара
+        /// </summary>
         public string Name => _product.Name;
 
+        /// <summary>
+        /// Описание товара
+        /// </summary>
         public string Description => _product.Description;
 
+        /// <summary>
+        /// Скидка на товар
+        /// </summary>
         public double Discount => _product.Discount;
 
+
+        /// <summary>
+        /// Оставшееся количество товара
+        /// </summary>
         public double ProductCount => _product.ProductCount;
 
+        /// <summary>
+        /// Изобращения товара
+        /// </summary>
         public ICollection<ProductImage> Images => _product.Images;
 
         public override string ToString()
