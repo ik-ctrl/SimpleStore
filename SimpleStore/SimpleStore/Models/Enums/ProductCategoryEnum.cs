a7f00712e75a2d0af32c5df3f79bbf1272c5f9b9
KondratievIE <kondratyev.ie@gmail.com>
2021-07-05 13:37:29 +0300

    enum для отображения типов товаров

---
+++ SimpleStore/SimpleStore/Models/Enums/ProductCategoryEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleStore.Models.Enums
+{
+    /// <summary>
+    /// Представление товаров для фильтрации 
+    /// </summary>
+    public enum ProductCategoryEnum
+    {
+        All=-1,
+        Books = 0,
+        Electronics = 10,
+        Wear = 20,
+        Sports = 30,
+        Footwear = 40,
+    }
+}
